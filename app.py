6712c4faf2c6d643dfa11d97e47e448ed814d99e
Amyh97 <amy.hollis@btinternet.com>
2020-10-11 12:28:45 +0000

    add template and links for materialize

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello world... again'
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = "mongodb+srv://avhollis97:Re67Whj@myfirstcluster.cz5rs.mongodb.net/task_manager?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 
-if __name__ == '__main__':
-    app.run(host =os.environ.get('IP'),
-            port =int(os.environ.get('PORT')),
-            debug =True)
+@app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
