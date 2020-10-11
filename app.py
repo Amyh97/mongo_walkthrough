79399f16ce7e97b3d37de9c1c36fc32397ba4a5e
Amyh97 <amy.hollis@btinternet.com>
2020-10-11 17:34:44 +0000

    add CRUD functionality

--- app.py
+++ app.py
@@ -19,18 +19,46 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
 @app.route("/add_tasks")
 def add_task():
     return render_template("addtasks.html",
-                            categories =mongo.db.categories.find())
+                            categories=mongo.db.categories.find())
 
 
 @app.route("/insert_task", methods=["POST"])
 def insert_task():
-    tasks =mongo.db.tasks
+    tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+
+@app.route("/edit_task/<task_id>")
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template("edittask.html", task=the_task, categories=all_categories)
+
+
+@app.route("/update_task/<task_id>", methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+
+    return redirect(url_for('get_tasks'))
+
+
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id':ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
