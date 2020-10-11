52d889f87b87cd35f9e5334c778ed36ae49214be
Amyh97 <amy.hollis@btinternet.com>
2020-10-11 14:44:01 +0000

    add task and view task functionality added

--- app.py
+++ app.py
@@ -19,6 +19,18 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route("/add_tasks")
+def add_task():
+    return render_template("addtasks.html",
+                            categories =mongo.db.categories.find())
+
+
+@app.route("/insert_task", methods=["POST"])
+def insert_task():
+    tasks =mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
